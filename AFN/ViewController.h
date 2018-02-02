86d952ccb702bb894164c28152759dd1a7a19a15
wxlian <wxlian_shirley@163.com>
2018-02-02 17:26:24 +0800

    Initial Commit

---
+++ AFN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFN
+//
+//  Created by zhangwei Luo on 2018/2/2.
+//  Copyright © 2018年 wxLian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AFN/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AFN
+//
+//  Created by zhangwei Luo on 2018/2/2.
+//  Copyright © 2018年 wxLian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
